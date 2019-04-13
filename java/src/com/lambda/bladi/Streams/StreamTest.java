b3d02e454476a638025197ce5850d5f7c9a466a8
BladimirDelBarrioPizarro <bladimaltego@gmail.com>
2019-04-13 13:20:37 +0200

    FlatMap added

--- java/src/com/lambda/bladi/Streams/StreamTest.java
+++ java/src/com/lambda/bladi/Streams/StreamTest.java
@@ -1,9 +1,10 @@
 package com.lambda.bladi.Streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class StreamTest {
 
@@ -12,11 +13,11 @@ public class StreamTest {
 
     public static  void setUpUser(){
         users = new ArrayList<>();
-        users.add(new User(1,"Bladi"));
+        users.add(new User(1,"Orco"));
         users.add(new User(2,"Pepe"));
-        users.add(new User(3,"Ale"));
+        users.add(new User(3,"Bladi"));
         users.add(new User(4,"Angela"));
-        users.add(new User(5,"Roberto"));
+        users.add(new User(5,"Bladi"));
         users.add(new User(6,"Mama"));
     }
 
@@ -52,10 +53,26 @@ public class StreamTest {
                                      .collect(Collectors.toList());
         userFilter.stream().forEach(item -> System.out.println("Id: "+item.getId()+"\nName: "+item.getNombre()));
 
+        System.out.println("------- FIND FIRST ---------------");
+        setUpUser();
+        //orElse en el caso de que el filtro no tenga Orco retorna el nuevo usuario
+        User user = users.stream()
+                        .filter(item -> item.getNombre().equals("Bladi"))
+                        .findFirst()
+                        .orElse(new User (7,"OrElse"));
+        System.out.println(user.getId()+" "+user.getNombre());
 
-
-
+        System.out.println("------- FLAT MAP ---------------");
+        //Obtiene los datos de diferentes arrays y los concatena en un único stream
+        //Una vez recopilados todos los datos en un mismo flujo podremos filtrar o lo que queramos.
+        List<List<String>> allList = new ArrayList<List<String>>(
+                Arrays.asList(
+                        new ArrayList<String>(Arrays.asList("Bladi","Rober","Pili")),
+                        new ArrayList<String>(Arrays.asList("Nocillo","Jaime","Guindi"))));
+        List<String> nameUniqueList = allList.stream()
+                .flatMap(item -> item.stream())
+                .collect(Collectors.toList());
+        nameUniqueList.stream().forEach(item -> System.out.println(item));
     }
-
 
 }
